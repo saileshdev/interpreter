662de83eee08e0921f117c0b7b56feeb1ec9e9dd
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-18 13:16:52 +0530

    Add Token class to represent individual tokens

--- src/interpreter.py
+++ src/interpreter.py
@@ -12,6 +12,22 @@ RESERVED_KEYWORDS = {
     'END': Token('END', 'END'),
 }
 
+
+class Token(object):
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return 'Token({type}, {value})'.format(
+            type=self.type,
+            value=repr(self.value)
+        )
+
+    def __repr__(self):
+        return self.__str__()
+
+
 def main():
     import sys
 
