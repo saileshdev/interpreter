7f69f8ca51cba7edc819de3f5d8aecb50d97dc51
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-18 13:25:05 +0530

    Add code to represent variables

--- src/interpreter.py
+++ src/interpreter.py
@@ -160,6 +160,15 @@ class Assign(AST):
         self.token = self.op = op
         self.right = right
 
+class Var(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+class NoOp(AST):
+    pass
+
+
 def main():
     import sys
 
