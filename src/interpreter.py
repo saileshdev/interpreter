42c722f528d5c78de86e1dcdb1d6adabec7fdb83
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-18 13:11:50 +0530

    Define the token types for the lexer

---
+++ src/interpreter.py
@@ -0,0 +1,7 @@
+# TOKEN TYPES
+
+(INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, ID, ASSIGN,
+ BEGIN, END, SEMI, DOT, EOF) = (
+    'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', '(', ')', 'ID', 'ASSIGN',
+    'BEGIN', 'END', 'SEMI', 'DOT', 'EOF'
+)
