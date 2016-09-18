ade66c33ab9821ce20450574bc48d44a9d215d4c
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-18 13:23:24 +0530

    Represent assignment opeartor

--- src/interpreter.py
+++ src/interpreter.py
@@ -154,6 +154,12 @@ class Compound(AST):
     def __init__(self):
         self.children = []
 
+class Assign(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
 def main():
     import sys
 
